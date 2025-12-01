10e3707f23b3c6d2a50ce2bb59b9266b80abd1ac
darius-konsebo <dariuskonsebo@gmail.com>
2025-12-01 17:30:40 +0100

    Création add command

---
+++ src/main/java/org/example/demoescqrsaxon/commands/commands/AddAccountCommand.java
@@ -0,0 +1,13 @@
+package org.example.demoescqrsaxon.commands.commands;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Getter @AllArgsConstructor
+public class AddAccountCommand {
+    @TargetAggregateIdentifier
+    private String id;
+    private double initialBalance;
+    private String currency;
+}
--- src/main/java/org/example/demoescqrsaxon/commands/controllers/AccountCommandController.java
+++ src/main/java/org/example/demoescqrsaxon/commands/controllers/AccountCommandController.java
@@ -1,5 +1,32 @@
 package org.example.demoescqrsaxon.commands.controllers;
-@RestController
 
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.example.demoescqrsaxon.commands.commands.AddAccountCommand;
+import org.example.demoescqrsaxon.commands.dtos.AddNewAccountRequestDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/commands")
 public class AccountCommandController {
+    private CommandGateway commandGateway;
+
+    public AccountCommandController(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+    }
+
+    @PostMapping("/add")
+    public CompletableFuture<String> addNewAccountCommand(@RequestBody AddNewAccountRequestDTO request){
+        CompletableFuture<String> response = commandGateway.send(new AddAccountCommand(
+                UUID.randomUUID().toString(),
+                request.initialBalance(),
+                request.currency()
+        ));
+        return response;
+    }
 }
